eee02ec586ba9c5095c7952ea16bf494ae21167e
Lucas Wojciechowski <lucas@mapbox.com>
2016-10-21 14:09:49 -0700

    Combine regexes and use "search" instead of "match"

--- js/data/bucket/symbol_bucket.js
+++ js/data/bucket/symbol_bucket.js
@@ -264,10 +264,7 @@ class SymbolBucket extends Bucket {
 
             let shapedTextOrientations;
             if (feature.text) {
-                const requiresVerticalWritingMode = (
-                    feature.text.match(/[\u1100-\u11FF\uAC00-\uD7A3\u3131-\u318E\u4E00-\u9FCC\u3400-\u4DB5\u3000-\u303F\uFF01-\uFFEE\u3041-\u309F\u30A0-\u30FF\u31F0-\u31FF\uA000-\uA4C6\u1800-\u18AA]/) ||
-                    feature.text.match(/(\uD840[\uDC00-\uFFFF])|[\uD841-\uD872]|(\uD873[\u0000-\uDEAF])/)
-                );
+                const requiresVerticalWritingMode = feature.text.search(/[\u1100-\u11FF\uAC00-\uD7A3\u3131-\u318E\u4E00-\u9FCC\u3400-\u4DB5\u3000-\u303F\uFF01-\uFFEE\u3041-\u309F\u30A0-\u30FF\u31F0-\u31FF\uA000-\uA4C6\u1800-\u18AA]|(\uD840[\uDC00-\uFFFF])|[\uD841-\uD872]|(\uD873[\u0000-\uDEAF])/);
 
                 shapedTextOrientations = {
                     [WritingMode.horizontal]: shapeText(feature.text, stacks[fontstack], maxWidth, lineHeight, horizontalAlign, verticalAlign, justify, spacing, textOffset, oneEm, WritingMode.horizontal),
