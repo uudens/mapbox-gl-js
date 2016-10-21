bf85ddc1619ed4e9d11939c431162d6673cf5ee8
Lucas Wojciechowski <lucas@mapbox.com>
2016-10-21 14:39:33 -0700

    Made script detection regex more readable 📚

--- js/util/script_detection.js
+++ js/util/script_detection.js
@@ -1,21 +1,22 @@
 'use strict';
 
+// please keep the comments below up to date!
+
 const isVerticalWritingModeRegExp = new RegExp([
-    '[\u1100-\u11FF]',
-    '[\uAC00-\uD7A3]',
-    '[\u3131-\u318E]',
-    '[\u4E00-\u9FCC]',
-    '[\u3400-\u4DB5]',
-    '[\u3000-\u303F]',
-    '[\uFF01-\uFFEE]',
-    '[\u3041-\u309F]',
-    '[\u30A0-\u30FF]',
-    '[\u31F0-\u31FF]',
-    '[\uA000-\uA4C6]',
-    '[\u1800-\u18AA]',
-    '\uD840[\uDC00-\uFFFF]',
-    '[\uD841-\uD872]',
-    '\uD873[\u0000-\uDEAF]'
+    '[ᄀ-ᇿ]',
+    '[가-힣]',
+    '[ㄱ-ㆎ]',
+    '[一-鿌]',
+    '[㐀-䶵]',
+    // eslint-disable-next-line no-irregular-whitespace
+    '[　-〿]',
+    '\uD840[\uDC00-\uFFFF]|[\uD841-\uD872]|\uD873[\u0000-\uDEAF]', // '[𠀀-𬺯]'
+    '[！-￮]',
+    '[ぁ-ゟ]',
+    '[゠-ヿ]',
+    '[ㇰ-ㇿ]',
+    '[ꀀ-꓆]',
+    '[᠀-ᢪ]'
 ].join('|'));
 
 module.exports.allowsVerticalWritingMode = function(input) {
