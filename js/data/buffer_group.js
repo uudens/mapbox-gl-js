642969da8e2f479fa6e786872e500032c89429a2
Molly Lloyd <molly@mapbox.com>
2016-12-12 10:40:22 -0500

    transfer terrain data to main thread for rendering

--- js/data/buffer_group.js
+++ js/data/buffer_group.js
@@ -30,6 +30,7 @@ class BufferGroup {
 
         this.segments = arrays.segments;
         this.segments2 = arrays.segments2;
+        this.terrainArray = arrays.terrainArray;
 
         for (const segments of [this.segments, this.segments2]) {
             for (const segment of segments || []) {
--- js/source/raster_tile_source.js
+++ js/source/raster_tile_source.js
@@ -5,6 +5,7 @@ const ajax = require('../util/ajax');
 const Evented = require('../util/evented');
 const loadTileJSON = require('./load_tilejson');
 const normalizeURL = require('../util/mapbox').normalizeTileURL;
+const {DEMPyramid, Level} = require('../geo/dem_pyramid');
 
 class RasterTileSource extends Evented {
 
@@ -48,7 +49,6 @@ class RasterTileSource extends Evented {
 
     loadTile(tile, callback) {
         const url = normalizeURL(tile.coord.url(this.tiles, null, this.scheme), this.url, this.tileSize);
-
         tile.request = ajax.getImage(url, done.bind(this));
 
         function done(err, img) {
@@ -65,11 +65,18 @@ class RasterTileSource extends Evented {
             }
 
             const gl = this.map.painter.gl;
+            var dem;
+            if (this.id==='terrain') {
+                dem = new DEMPyramid();
+                dem.loadFromImage(img);
+            }
+
             tile.texture = this.map.painter.getTileTexture(img.width);
             if (tile.texture) {
                 gl.bindTexture(gl.TEXTURE_2D, tile.texture);
-                gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, img);
+                gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, dem ? dem.levels[0].data : img);
             } else {
+                console.log(dem.levels[0].data);
                 tile.texture = gl.createTexture();
                 gl.bindTexture(gl.TEXTURE_2D, tile.texture);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
@@ -77,10 +84,11 @@ class RasterTileSource extends Evented {
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
                 gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
-                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-                tile.texture.size = img.width;
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, dem ? dem.levels[0].data : img);
+                tile.texture.size = dem ? dem.levels[0].width : img.width;
             }
             gl.generateMipmap(gl.TEXTURE_2D);
+
 
             tile.state = 'loaded';
 
